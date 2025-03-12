da97f9d2f0eb9bdd78e234921334499eb7b680b1
evgeny3000 <AlekseevE2015@yandex.ru>
2025-03-12 22:58:37 +0300

    feat: employee crud

--- Homeworks/Base/src/PromoCodeFactory.DataAccess/Repositories/InMemoryRepository.cs
+++ Homeworks/Base/src/PromoCodeFactory.DataAccess/Repositories/InMemoryRepository.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using PromoCodeFactory.Core.Abstractions.Repositories;
 using PromoCodeFactory.Core.Domain;
+using PromoCodeFactory.Core.Domain.Administration;
 namespace PromoCodeFactory.DataAccess.Repositories
 {
     public class InMemoryRepository<T>: IRepository<T> where T: BaseEntity
     {
         protected IEnumerable<T> Data { get; set; }
+
+        protected IEnumerable<T> GetRepositoryWithoutId(Guid id) => Data.Where(x => !x.Id.Equals(id));        
 
         public InMemoryRepository(IEnumerable<T> data)
         {
@@ -23,6 +26,41 @@ namespace PromoCodeFactory.DataAccess.Repositories
         public Task<T> GetByIdAsync(Guid id)
         {
             return Task.FromResult(Data.FirstOrDefault(x => x.Id == id));
+        }
+
+        public Task SetAsync(IEnumerable<T> items)
+        {
+            Data = items;
+            return Task.FromResult<object>(null);
+        }
+
+        public Task<T> AddAsync(T item)
+        {
+            if (item == null) 
+                throw new ArgumentNullException(nameof(item));            
+            
+            return Task.Run(() => {
+                if (Data.SingleOrDefault(x => x.Id.Equals(item.Id)) != null)
+                    throw new ArgumentException($"Идентификатор '{item.Id}' должен быть уникальным!");
+
+                Data = Data.Append(item);
+
+                return item;
+            });
+        }
+
+        public Task DeleteByIdAsync(Guid id)
+        {
+            return Task.Run(() => Data = GetRepositoryWithoutId(id));
+        }
+
+        public Task UpdateAsync(T item)
+        {
+            return Task.Run(() =>
+            {                
+                Data = GetRepositoryWithoutId(item.Id).Append(item);
+                return;
+            });
         }
     }
 }
